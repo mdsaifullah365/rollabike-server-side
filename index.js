5d59c6085fe4d7994bb51b768583e0f291defdd4
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2022-05-23 02:17:17 +0600

    create verifyToken middleware

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors());
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
+// MongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ds8m7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -32,11 +47,17 @@ async function run() {
     });
 
     // Get One Product
-    app.get("/product/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await productCollection.findOne(query);
-      res.send(result);
+    app.get("/product/:id", verifyToken, async (req, res) => {
+      const tokenEmail = req.decoded.email;
+      const userEmail = req.query.email;
+      if (tokenEmail === userEmail) {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await productCollection.findOne(query);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "Forbidden Access" });
+      }
     });
 
     // Update a Product
@@ -59,11 +80,11 @@ async function run() {
       const result = await orderCollection.insertOne(order);
       res.send(result);
     });
+
     // Update User and generate a JWT Token
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
-      console.log(user);
       const filter = { email: email };
       const options = { upsert: true };
       const updateDoc = {

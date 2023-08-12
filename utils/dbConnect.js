d0855e78f13ca671efc1857aef7275b6d45102a0
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2023-08-12 08:48:05 +0600

    update connection string variable

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ds8m7.mongodb.net/?retryWrites=true&w=majority`;
+const connectionString = process.env.DB_CONNECTION_STRING;
 
 const client = new MongoClient(connectionString, {
   useNewUrlParser: true,
